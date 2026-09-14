5bb130399488a16bba9865a364d181feab4da8b9
Fibie Kalis <fibie@bu.edu>
2026-09-14 12:56:13 -0400

    Created the python lists for quotes and images in views.py. And created the quote function which selects a random quote and image from the lists and delegates it to a template. This template will be quotes.html (not yet created).

--- cs412/urls.py
+++ cs412/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hw/', include('hw.urls')),
-    path('quotes/', include('quotes.urls')),
+    path('quotes/', include('quotes.urls')), #Connecting quotes/urls.py to main urls.py for A1
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- quotes/views.py
+++ quotes/views.py
@@ -3,25 +3,45 @@
 
 
 from django.shortcuts import render
-# Classes that will be helpful in creating our function
-from django.http import HttpRequest, HttpResponse
+import random   # Allows us to pick a random quote and image from python lists 
 
 # Create your views here.
 
+# List of quotes (string)
+quotes = [
+    "I want to feel my life while I'm in it.",
+    "Integrate what you believe in every single area of your life. Take your heart to work and ask the most and best of everybody else, too",
+    "The great gift of human beings is that we have the power of empathy, we can all sense a mysterious connection to each other.",
+    "I'm curious about other people. That's the essence of my acting. I'm interested in what it would be like to be you.",
+    "What does it take to be the first female anything? It takes grit, and it takes grace.",
+    "The interesting thing about being a mother is that everyone wants pets, but no one but me cleans the kitty litter.",
+    "I think the best role models for women are people who are fruitfully and confidently themselves, who bring light into the world.",
+    "It is well that the earth is round that we do not see too far ahead.",
+    "You can't get spoiled if you do your own ironing.",
+    "It's amazing what you can get if you quietly, clearly, and authoritatively demand it."
+]
+
+# List of images (URLs as strings)
+images = [
+    "https://media.newyorker.com/photos/59097660ebe912338a377be8/1:1/w_1304,h_1304,c_limit/Schulman-Meryl-Streeps-Twenties1.jpg"
+]
+
+
+
 # Functions that will respond to our http requests and create a response
 def quote(request): 
-    '''Function to respond to the 'home' request.'''
+    '''Function to respond to the 'quote' request.
+       Select a random quote and image, and delegate to a template.'''
+    
+    random_quote = random.choice(quotes)
+    random_image = random.choice(images)
 
-    response_text = '''
-    <html>
-        <h1>Hello, world!</h1>
-    </html>
-    '''
+    display = {
+        'quote' : random_quote,     # Randomly picks one quote from the list
+        'image' : random_image      # Randomly picks one image from the list 
+    }
+    # Values used in quotes.html
 
-    return HttpResponse(response_text)
+    template_name = 'quotes/quote.html'
 
-def home_page(request):
-    ''' Respond to the URL '', delegate work to a template '''
-
-    template_name = 'quotes/base.html'
-    return render(request, template_name)
+    return render(request, template_name, display)
