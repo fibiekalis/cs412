045b3449c85fe44002a88b4f7c706bcb56e909db
Fibie Kalis <fibie@bu.edu>
2026-09-14 11:56:24 -0400

    Created quotes URL and quotes view. Incomplete as the page is currently still empty.

---
+++ quotes/urls.py
@@ -0,0 +1,14 @@
+# File: quotes/urls.py
+# Author: Fibie Kalis (fibie@bu.edu), 9/10/2026
+# Description: file containing all the URL patterns for the 'quotes' django app 
+
+from django.urls import path
+from django.conf import settings 
+from . import views 
+
+# URL patterns specific to the quotes app
+urlpatterns = [
+    # URL pattern for the main page with the image and quote 
+    path(r'', views.quote, name="quote"),
+
+]
--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,27 @@
+# File: quotes/views.py
+# Author: Fibie Kalis (fibie@bu.edu), 9/10/2026
+
+
 from django.shortcuts import render
+# Classes that will be helpful in creating our function
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+# Functions that will respond to our http requests and create a response
+def quote(request): 
+    '''Function to respond to the 'home' request.'''
+
+    response_text = '''
+    <html>
+        <h1>Hello, world!</h1>
+    </html>
+    '''
+
+    return HttpResponse(response_text)
+
+def home_page(request):
+    ''' Respond to the URL '', delegate work to a template '''
+
+    template_name = 'quotes/base.html'
+    return render(request, template_name)
